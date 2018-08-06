1b4b3e616bacb4c2e2fcbf575d10de41c06c692e
Chaouki Dhib <chaodhib@gmail.com>
2018-08-06 15:43:51 +0200

    clean up

--- src/main/java/com/chaouki/tcshop/dao/CharacterEquipmentDao.java
+++ src/main/java/com/chaouki/tcshop/dao/CharacterEquipmentDao.java
@@ -2,11 +2,12 @@ package com.chaouki.tcshop.dao;
 
 import com.chaouki.tcshop.entities.Character;
 import com.chaouki.tcshop.entities.CharacterEquipment;
+import com.chaouki.tcshop.entities.CharacterEquipmentSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CharacterEquipmentDao extends JpaRepository<CharacterEquipment, Integer> {
+public interface CharacterEquipmentDao extends JpaRepository<CharacterEquipment, CharacterEquipmentSlot> {
 
     @Query("DELETE FROM CharacterEquipment WHERE slot.character = ?1")
     @Modifying
