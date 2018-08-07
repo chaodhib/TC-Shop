9c99bee9be5f477d08f4561ec9b28c3256ce6982
Chaouki Dhib <chaodhib@gmail.com>
2018-08-07 16:58:41 +0200

    add filters on entry, name & class for item list view

--- src/main/java/com/chaouki/tcshop/controllers/ItemController.java
+++ src/main/java/com/chaouki/tcshop/controllers/ItemController.java
@@ -3,6 +3,7 @@ package com.chaouki.tcshop.controllers;
 import com.chaouki.tcshop.entities.Account;
 import com.chaouki.tcshop.entities.Character;
 import com.chaouki.tcshop.entities.ItemTemplate;
+import com.chaouki.tcshop.entities.enums.ItemClass;
 import com.chaouki.tcshop.services.AccountService;
 import com.chaouki.tcshop.services.CharacterService;
 import com.chaouki.tcshop.services.ItemTemplateService;
@@ -14,8 +15,11 @@ import org.springframework.security.core.userdetails.User;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
+import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ManagedBean
 @ViewScoped
@@ -25,13 +29,29 @@ public class ItemController {
     private ItemTemplateService itemTemplateService;
 
     private LazyDataModel<ItemTemplate> itemTemplates;
+    private List<ItemTemplate> filteredItems;
+    private List<SelectItem> itemClassOptions;
 
     @PostConstruct
     public void init(){
         itemTemplates = new ItemLazyDataModel(itemTemplateService);
+        itemClassOptions = Arrays.stream(ItemClass.values()).map(itemClass -> new SelectItem(itemClass.getIdx(),itemClass.getLabel())).collect(Collectors.toList());
+        itemClassOptions.add(0, new SelectItem(""));
     }
 
     public LazyDataModel<ItemTemplate> getItemTemplates() {
         return itemTemplates;
+    }
+
+    public List<ItemTemplate> getFilteredItems() {
+        return filteredItems;
+    }
+
+    public void setFilteredItems(List<ItemTemplate> filteredItems) {
+        this.filteredItems = filteredItems;
+    }
+
+    public List<SelectItem> getItemClassOptions() {
+        return itemClassOptions;
     }
 }
--- src/main/java/com/chaouki/tcshop/controllers/ItemLazyDataModel.java
+++ src/main/java/com/chaouki/tcshop/controllers/ItemLazyDataModel.java
@@ -1,13 +1,20 @@
 package com.chaouki.tcshop.controllers;
 
 import com.chaouki.tcshop.entities.ItemTemplate;
+import com.chaouki.tcshop.entities.enums.ItemClass;
 import com.chaouki.tcshop.services.ItemTemplateService;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortMeta;
 import org.primefaces.model.SortOrder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +28,33 @@ public class ItemLazyDataModel extends LazyDataModel<ItemTemplate> {
 
     @Override
     public List<ItemTemplate> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-        Page<ItemTemplate> page = itemTemplateService.findAll(null, PageRequest.of(first/pageSize, pageSize));
+
+        Specification<ItemTemplate> spec = null;
+        if(!filters.isEmpty()) {
+            spec = (Specification<ItemTemplate>) (root, query, criteriaBuilder) -> {
+                List<Predicate> predicateList = new ArrayList<>();
+
+                if(filters.containsKey("entry")) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("entry"), filters.get("entry"));
+                    predicateList.add(predicate);
+                }
+
+                if(filters.containsKey("name")) {
+                    Predicate predicate = criteriaBuilder.like(root.get("name"), "%" + filters.get("name") + "%");
+                    predicateList.add(predicate);
+                }
+
+                if(filters.containsKey("itemClass.idx")) {
+                    Predicate predicate = criteriaBuilder.equal(root.get("itemClass"), ItemClass.getByIndex(Integer.valueOf((String) filters.get("itemClass.idx"))));
+                    predicateList.add(predicate);
+                }
+
+                return criteriaBuilder.and(predicateList.toArray(new Predicate[0]));
+            };
+        }
+
+        Page<ItemTemplate> page = itemTemplateService.findAll(spec, PageRequest.of(first / pageSize, pageSize));
+
         this.setRowCount(Long.valueOf(page.getTotalElements()).intValue());
         this.setPageSize(page.getNumberOfElements());
         return page.getContent();
