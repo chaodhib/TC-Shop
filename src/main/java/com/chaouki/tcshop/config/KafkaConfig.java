1c898f864c2a1e8da71c439a098710158bff5311
Chaouki Dhib <chaodhib@gmail.com>
2018-08-21 16:36:41 +0200

    lower kafka thread pool since we only have one consumer now instead of 4

--- src/main/java/com/chaouki/tcshop/config/KafkaConfig.java
+++ src/main/java/com/chaouki/tcshop/config/KafkaConfig.java
@@ -11,7 +11,7 @@ public class KafkaConfig {
     @Bean
     public AsyncTaskExecutor taskExecutor(){
         SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
-        taskExecutor.setConcurrencyLimit(5);
+        taskExecutor.setConcurrencyLimit(2);
         return taskExecutor;
     }
 }
