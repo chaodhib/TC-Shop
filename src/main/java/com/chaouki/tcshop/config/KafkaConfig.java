a53467bb4058ec4af8c92b27f42c5bc0eaaddeba
Chaouki Dhib <chaodhib@gmail.com>
2018-08-23 02:22:32 +0200

    readd config annotation for scheduler

--- src/main/java/com/chaouki/tcshop/config/KafkaConfig.java
+++ src/main/java/com/chaouki/tcshop/config/KafkaConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
+@EnableScheduling
 public class KafkaConfig {
 
     @Bean
