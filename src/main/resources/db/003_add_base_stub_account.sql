772925ebbd36a723ca6247964876dec46be0e525
Chaouki Dhib <chaodhib@gmail.com>
2018-08-24 11:45:29 +0200

    clean up SQL scripts

---
+++ src/main/resources/db/003_add_base_stub_account.sql
@@ -0,0 +1,2 @@
+INSERT INTO account (id, hashed_password, username, last_update_timestamp, is_stub)
+    VALUE (2147483647, 'STUB@ACCOUNT', 'STUB@ACCOUNT', '2000-01-01 00:00:00', 1); # id = max int value
